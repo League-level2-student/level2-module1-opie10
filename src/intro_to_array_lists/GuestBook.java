37c45340a9d7035755b8f8f5c49a209b3a111b62
augie <augie@keatinge.com>
2020-12-03 18:30:25 -0800

    Intro to Arrays
    Started guest bbok, but switched to Intro to arrays, on #5 or 6

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,51 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.peer.ButtonPeer;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	JFrame jf = new JFrame() ;
+	JPanel jp = new JPanel() ;
+	JButton an = new JButton() ;
+	JButton vn = new JButton() ;
+	
+	void run() {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
+	jf.add(jp);
+  jp.add(an);
+  jp.add(vn);
+  jf.setVisible(true);
+  an.addActionListener(this);
+  vn.addActionListener(this);
+  an.setText("Add Name");
+  vn.setText("View Names");
 	// Guest #1: Bob Banders
+	
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+  
+  
 }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton bp = (JButton)e.getSource();
+		if (bp == an){
+		String IN	= JOptionPane.showInputDialog("Enter a name to add to the list");
+		
+		}
+		else  {
+			
+		}
+		}
+	}
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,16 +1,26 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> Intro = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		Intro.add("hi");
+		Intro.add("hoi");
+		Intro.add("hello");
+		Intro.add("why hello there");
+		Intro.add("ih");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i <Intro.size(); i++) {
+			System.out.println(Intro.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for (String fel : Intro) {
+			System.out.println(fel);
+		}
 		//5. Print only the even numbered elements in the list.
 		
 		//6. Print all the Strings in reverse order.
